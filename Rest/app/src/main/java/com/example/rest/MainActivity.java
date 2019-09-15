2001176ce137542c54e78125c8263e5d48c7a3fd
panther_28 <playkavinda@gmail.com>
2019-09-15 18:26:18 +0530

    Database, SQLite and minor ui changes

--- Rest/app/src/main/java/com/example/rest/MainActivity.java
+++ Rest/app/src/main/java/com/example/rest/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.rest;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    dbhelper Mydb;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Mydb = new dbhelper(this);
+
+        Button button = (Button) findViewById(R.id.buttonOk);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity2();
+            }
+        });
+
 
     }
+        public void activity2() {
+            Intent intent = new Intent(this, activity2.class);
+            startActivity(intent);
+        }
+
 }
--- Rest/app/src/main/java/com/example/rest/activity2.java
+++ Rest/app/src/main/java/com/example/rest/activity2.java
@@ -3,11 +3,11 @@ package com.example.rest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class page2 extends AppCompatActivity {
+public class activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page2);
+        setContentView(R.layout.activity2);
     }
 }
